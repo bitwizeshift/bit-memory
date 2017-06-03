1235154cab6a239bc2a938b55819b9117d25c63d
Matthew Rodusek <matthew.rodusek@gmail.com>
2017-06-03 14:06:30 -0400

    Added new_block_allocator that uses ::operator new

---
+++ include/bit/memory/new_block_allocator.hpp
@@ -0,0 +1,64 @@
+/**
+ * \file new_block_allocator.hpp
+ *
+ * \brief This header contains an API for allocating memory blocks that use
+ *        ::operator new as the underlying allocation scheme.
+ *
+ * \author Matthew Rodusek (matthew.rodusek@gmail.com)
+ */
+
+#ifndef BIT_MEMORY_NEW_BLOCK_ALLOCATOR_HPP
+#define BIT_MEMORY_NEW_BLOCK_ALLOCATOR_HPP
+
+#include "memory.hpp"
+#include "memory_block.hpp"
+
+namespace bit {
+  namespace memory {
+
+    //////////////////////////////////////////////////////////////////////////
+    /// \brief A block allocator that uses ::operator new(...) to allocate
+    ///        memory blocks
+    ///
+    /// \satisfies BlockAllocator
+    //////////////////////////////////////////////////////////////////////////
+    class new_block_allocator
+    {
+      //----------------------------------------------------------------------
+      // Constructors
+      //----------------------------------------------------------------------
+    public:
+
+      /// \brief Constructs a new_block_allocator that will distribute
+      ///        blocks of the specified \p size
+      ///
+      /// \param size the size of each block allocation
+      explicit new_block_allocator( std::size_t size ) noexcept;
+
+      //----------------------------------------------------------------------
+      // Block Allocations
+      //----------------------------------------------------------------------
+    public:
+
+      /// \brief Allocates a new'ed memory_block
+      ///
+      /// \return a null memory_block
+      owner<memory_block> allocate_block();
+
+      /// \brief Deallocates a new'ed memory_block
+      ///
+      /// \param block the block to deallocate
+      void deallocate_block( owner<memory_block> block );
+
+      //----------------------------------------------------------------------
+      // Private Members
+      //----------------------------------------------------------------------
+    private:
+
+      std::size_t m_size;
+    };
+
+  } // namespace memory
+} // namespace bit
+
+#endif /* BIT_MEMORY_NEW_BLOCK_ALLOCATOR_HPP */
---
+++ src/bit/memory/new_block_allocator.cpp
@@ -0,0 +1,14 @@
+#include <bit/memory/new_block_allocator.hpp>
+
+#include <new> // ::new
+
+bit::memory::owner<bit::memory::memory_block>
+  bit::memory::new_block_allocator::allocate_block()
+{
+  return memory_block( ::operator new(m_size), m_size );
+}
+
+void bit::memory::new_block_allocator::deallocate_block( owner<memory_block> block )
+{
+  ::operator delete( block.data() );
+}
