4d41dfc66d7d4a020671b7f00762749b05d51146
Matthew Rodusek <matthew.rodusek@gmail.com>
2017-06-10 20:28:30 -0400

    Added tests for malloc_block_allocator

--- src/bit/memory/malloc_block_allocator.cpp
+++ src/bit/memory/malloc_block_allocator.cpp
@@ -2,6 +2,13 @@
 
 #include <cstdlib> // std::malloc
 
+bit::memory::malloc_block_allocator::malloc_block_allocator( std::size_t n )
+  noexcept
+  : m_size(n)
+{
+
+}
+
 bit::memory::owner<bit::memory::memory_block>
   bit::memory::malloc_block_allocator::allocate_block()
 {
