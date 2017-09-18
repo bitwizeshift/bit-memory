335fc3494cef96ee8eff3f34da459f36e1f4ea11
Matthew Rodusek <matthew.rodusek@gmail.com>
2017-09-18 15:04:59 -0400

    Added 'malloc_allocator' RawAllocator

---
+++ include/bit/memory/detail/malloc_allocator.inl
@@ -0,0 +1,57 @@
+#ifndef BIT_MEMORY_DETAIL_MALLOC_ALLOCATOR_INL
+#define BIT_MEMORY_DETAIL_MALLOC_ALLOCATOR_INL
+
+//-----------------------------------------------------------------------------
+// Allocation / Deallocation
+//-----------------------------------------------------------------------------
+
+inline void* bit::memory::malloc_allocator::allocate( std::size_t size,
+                                                      std::size_t align )
+{
+  auto p = try_allocate(size,align);
+
+  if( p == nullptr ) {
+    out_of_memory_handler( {"malloc_allocator", nullptr}, size );
+  }
+
+  return p;
+}
+
+inline void* bit::memory::malloc_allocator::try_allocate( std::size_t size,
+                                                          std::size_t align )
+  noexcept
+{
+  (void) align;
+
+  return std::malloc( size );
+}
+
+//-----------------------------------------------------------------------------
+
+inline void bit::memory::malloc_allocator::deallocate( void* p,
+                                                       std::size_t size )
+{
+  (void) size;
+
+  std::free( p );
+}
+
+//-----------------------------------------------------------------------------
+// Comparisons
+//-----------------------------------------------------------------------------
+
+inline bool bit::memory::operator==( const malloc_allocator&,
+                                     const malloc_allocator& )
+  noexcept
+{
+  return true;
+}
+
+inline bool bit::memory::operator!=( const malloc_allocator&,
+                                     const malloc_allocator& )
+  noexcept
+{
+  return false;
+}
+
+#endif /* BIT_MEMORY_DETAIL_MALLOC_ALLOCATOR_INL */
---
+++ include/bit/memory/malloc_allocator.hpp
@@ -0,0 +1,113 @@
+/**
+ * \file malloc_allocator.hpp
+ *
+ * \brief This header contains the definition for the RawAllocator,
+ *        malloc_allocator
+ *
+ * \author Matthew Rodusek (matthew.rodusek@gmail.com)
+ */
+#ifndef BIT_MEMORY_MALLOC_ALLOCATOR_HPP
+#define BIT_MEMORY_MALLOC_ALLOCATOR_HPP
+
+#include "debugging.hpp"
+
+#include <cstdlib>     // std::malloc, std::free
+#include <type_traits> // std::true_type
+
+namespace bit {
+  namespace memory {
+
+    ///////////////////////////////////////////////////////////////////////////
+    /// \brief This stateless allocator performs all of its allocation calls
+    ///        using raw calls to 'malloc', and frees memory with 'free'
+    ///
+    /// Any alignment requests are ignored
+    ///
+    /// \satisfies RawAllocator
+    ///////////////////////////////////////////////////////////////////////////
+    class malloc_allocator
+    {
+      //-----------------------------------------------------------------------
+      // Public Static Members
+      //-----------------------------------------------------------------------
+    public:
+
+      using is_always_equal = std::true_type;
+      using is_stateless    = std::true_type;
+
+      //-----------------------------------------------------------------------
+      // Constructor / Assignment
+      //-----------------------------------------------------------------------
+    public:
+
+      /// \brief Default-constructs a malloc_allocator
+      malloc_allocator() = default;
+
+      /// \brief Move-constructs a malloc_allocator from another allocator
+      ///
+      /// \param other the other malloc_allocator to move
+      malloc_allocator( malloc_allocator&& other ) noexcept = default;
+
+      // Deleted copy constructor
+      malloc_allocator( const malloc_allocator& other ) = delete;
+
+      //-----------------------------------------------------------------------
+
+      /// \brief Move-assigns a malloc_allocator from another allocator
+      ///
+      /// \param other the other allocator to move_assign
+      /// \return reference to \c (*this)
+      malloc_allocator& operator=( malloc_allocator&& other ) noexcept = default;
+
+      // Deleted copy assignment
+      malloc_allocator& operator=( const malloc_allocator& other ) = delete;
+
+      //-----------------------------------------------------------------------
+      // Allocations / Deallocation
+      //-----------------------------------------------------------------------
+    public:
+
+      /// \brief Allocates memory of size \p size
+      ///
+      /// The alignment is ignored for calls to this allocator. The alignment
+      /// is always guaranteed to be at least \c alignof(std::max_align_t)
+      ///
+      /// \param size the size of this allocation
+      /// \param align the requested alignment (ignored)
+      /// \return the allocated pointer
+      void* allocate( std::size_t size, std::size_t align );
+
+      /// \brief Attempts to allocate memory of size \p size, returning nullptr
+      ///        on failure
+      ///
+      /// The alignment is ignored for calls to this allocator. The alignment
+      /// is always guaranteed to be at least \c alignof(std::max_align_t)
+      ///
+      /// \param size the size of this allocation
+      /// \param align the requested alignment (ignored)
+      /// \return the allocated pointer
+      void* try_allocate( std::size_t size, std::size_t align ) noexcept;
+
+      /// \brief Deallocates a pointer \p p with the allocation size of \p size
+      ///
+      /// \param p the pointer to deallocate
+      /// \param size the size to deallocate
+      void deallocate( void* p, std::size_t size );
+    };
+
+    /// \{
+    /// \brief Compares equality between two malloc_allocators
+    ///
+    /// Two malloc_allocators are always considered the same
+    bool operator==( const malloc_allocator& lhs,
+                     const malloc_allocator& rhs ) noexcept;
+    bool operator!=( const malloc_allocator& lhs,
+                     const malloc_allocator& rhs ) noexcept;
+    /// \}
+
+  } // namespace memory
+} // namespace bit
+
+#include "detail/malloc_allocator.inl"
+
+#endif /* BIT_MEMORY_MALLOC_ALLOCATOR_HPP */
