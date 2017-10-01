fb1fc9319c7d3b620ffb0f54df560fb2f099f368
Matthew Rodusek <matthew.rodusek@gmail.com>
2017-09-30 20:17:54 -0400

    Added MSVC's __assume to macros.hpp

--- include/bit/memory/macros.hpp
+++ include/bit/memory/macros.hpp
@@ -34,8 +34,8 @@
 
 #if defined(__clang__)
 #define BIT_MEMORY_ASSUME(x) __builtin_assume(x)
-#elif defined(__GNUC__)
-#define BIT_MEMORY_ASSUME(x) __builtin_assume(x)
+#elif defined(_MSC_VER)
+#define BIT_MEMORY_ASSUME(x) __assume(x)
 #else
 #define BIT_MEMORY_ASSUME(x) x
 #endif
