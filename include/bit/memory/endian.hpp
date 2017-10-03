b03ca886a33d7f5470b7ebc55317734b9f78d54b
Matthew Rodusek <matthew.rodusek@gmail.com>
2017-10-02 20:02:05 -0400

    Temporary commit

---
+++ include/bit/memory/endian.hpp
@@ -0,0 +1,12 @@
+/**
+ * \file endian.hpp
+ *
+ * \brief TODO: Add description
+ *
+ * \author Matthew Rodusek (matthew.rodusek@gmail.com)
+ */
+#ifndef BIT_MEMORY_ENDIAN_HPP
+#define BIT_MEMORY_ENDIAN_HPP
+
+
+#endif /* BIT_MEMORY_ENDIAN_HPP */
