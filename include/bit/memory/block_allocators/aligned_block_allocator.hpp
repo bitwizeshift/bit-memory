a2c7b26f914463f652c3187d0ec7cb5c918e0d53
Matthew Rodusek <matthew.rodusek@gmail.com>
2017-10-04 21:52:24 -0400

    Added static and dynamic support for aligned_block_allocator
    The alignment and block size can now be specified at compile-time
    or runtime

--- include/bit/memory/block_allocators/aligned_block_allocator.hpp
+++ include/bit/memory/block_allocators/aligned_block_allocator.hpp
@@ -10,15 +10,104 @@
 #ifndef BIT_MEMORY_BLOCK_ALLOCATORS_ALIGNED_BLOCK_ALLOCATOR_HPP
 #define BIT_MEMORY_BLOCK_ALLOCATORS_ALIGNED_BLOCK_ALLOCATOR_HPP
 
-#include "../memory.hpp"             // is_power_of_two
-#include "../aligned_memory.hpp"     // aligned_allocate
-#include "../memory_block.hpp"       // memory_block
+#include "../detail/dynamic_size_type.hpp" // dynamic_size, detail::dynamic_size_type
 
-#include "cached_block_allocator.hpp"
-#include "debug_block_allocator.hpp"
+#include "../memory.hpp"         // is_power_of_two
+#include "../aligned_memory.hpp" // aligned_allocate
+#include "../memory_block.hpp"   // memory_block
+
+#include "cached_block_allocator.hpp" // cached_block_allocator
+#include "debug_block_allocator.hpp"  // debug_block_allocator
+
+#include <type_traits> // std::true_type, std::false_type, etc
+#include <cstddef>     // std::size_t
+#include <casser>      // assert
 
 namespace bit {
   namespace memory {
+    namespace detail {
+
+      template<std::size_t Size, std::size_t Align>
+      struct aligned_block_allocator_base
+        : detail::dynamic_size_type<0,Size>,
+          detail::dynamic_size_type<1,Align>
+      {
+        using block_size      = std::integral_constant<std::size_t,Size>;
+        using block_alignment = std::integral_constant<std::size_t,Align>;
+        using is_stateless    = std::true_type;
+
+        aligned_block_allocator_base() noexcept = default;
+        aligned_block_allocator_base( aligned_block_allocator_base&& ) noexcept = default;
+        aligned_block_allocator_base( const aligned_block_allocator_base& ) noexcept = default;
+        aligned_block_allocator_base& operator=( aligned_block_allocator_base&& ) noexcept = default;
+        aligned_block_allocator_base& operator=( const aligned_block_allocator_base& ) noexcept = default;
+      };
+
+      //-----------------------------------------------------------------------
+
+      template<std::size_t Size>
+      struct aligned_block_allocator_base<Size,dynamic_size>
+        : detail::dynamic_size_type<0,Size>,
+          detail::dynamic_size_type<1,dynamic_size>
+      {
+        using block_size   = std::integral_constant<std::size_t,Size>;
+        using is_stateless = std::false_type;
+
+        aligned_block_allocator_base( std::size_t align ) noexcept
+          : detail::dynamic_size_type<1,dynamic_size>( align )
+        {
+
+        }
+        aligned_block_allocator_base( aligned_block_allocator_base&& ) noexcept = default;
+        aligned_block_allocator_base( const aligned_block_allocator_base& ) = delete;
+        aligned_block_allocator_base& operator=( aligned_block_allocator_base&& ) = delete;
+        aligned_block_allocator_base& operator=( const aligned_block_allocator_base& ) = delete;
+      };
+
+      //-----------------------------------------------------------------------
+
+      template<std::size_t Align>
+      struct aligned_block_allocator_base<dynamic_size,Align>
+        : detail::dynamic_size_type<0,dynamic_size>,
+          detail::dynamic_size_type<1,Align>
+      {
+        using block_alignment = std::integral_constant<std::size_t,Align>;
+        using is_stateless    = std::false_type;
+
+        aligned_block_allocator_base( std::size_t size ) noexcept
+          : detail::dynamic_size_type<0,dynamic_size>( size )
+        {
+
+        }
+        aligned_block_allocator_base( aligned_block_allocator_base&& ) noexcept = default;
+        aligned_block_allocator_base( const aligned_block_allocator_base& ) = delete;
+        aligned_block_allocator_base& operator=( aligned_block_allocator_base&& ) = delete;
+        aligned_block_allocator_base& operator=( const aligned_block_allocator_base& ) = delete;
+      };
+
+      //-----------------------------------------------------------------------
+
+      template<>
+      struct aligned_block_allocator_base<dynamic_size,dynamic_size>
+        : detail::dynamic_size_type<0,dynamic_size>,
+          detail::dynamic_size_type<1,dynamic_size>
+      {
+        using is_stateless = std::false_type;
+
+        aligned_block_allocator_base( std::size_t size, std::size_t align ) noexcept
+          : detail::dynamic_size_type<0,dynamic_size>( size ),
+            detail::dynamic_size_type<1,dynamic_size>( align )
+        {
+          assert( is_power_of_two(align) && "Alignment must be a power of two!" );
+        }
+
+        aligned_block_allocator_base( aligned_block_allocator_base&& ) noexcept = default;
+        aligned_block_allocator_base( const aligned_block_allocator_base& ) = delete;
+        aligned_block_allocator_base& operator=( aligned_block_allocator_base&& ) = delete;
+        aligned_block_allocator_base& operator=( const aligned_block_allocator_base& ) = delete;
+      };
+
+    } // namespace detail
 
     //////////////////////////////////////////////////////////////////////////
     /// \brief A raw allocator that allocates over-aligned memory
@@ -31,15 +120,13 @@ namespace bit {
     //////////////////////////////////////////////////////////////////////////
     template<std::size_t Size,std::size_t Align>
     class aligned_block_allocator
+        : private detail::aligned_block_allocator_base<Size,Align>
     {
-      static_assert( is_power_of_two(Align), "Alignment must be a power of two!" );
+      using base_type = detail::aligned_block_allocator_base<Size,Align>;
+      using block_size_member  = detail::dynamic_size_type<0,Size>;
+      using block_align_member = detail::dynamic_size_type<1,Align>;
 
-      //----------------------------------------------------------------------
-      // Public Member Types
-      //----------------------------------------------------------------------
-    public:
-
-      using block_alignment = std::integral_constant<std::size_t,Align>;
+      static_assert( is_power_of_two(Align) || Align == dynamic_size, "Alignment must be a power of two!" );
 
       //----------------------------------------------------------------------
       // Constructors / Allocators
@@ -48,19 +135,20 @@ namespace bit {
 
       /// \brief Constructs a aligned_block_allocator that will distribute
       ///        blocks of the specified \p size
-      ///
-      /// \param size the size of each block allocation
-      aligned_block_allocator() noexcept = default;
+      aligned_block_allocator() = default;
+
+      // Inherit the constructors
+      using base_type::base_type;
 
       /// \brief Move-constructs an aligned_block_allocator from another allocator
       ///
       /// \param other the other aligned_block_allocator to move
-      aligned_block_allocator( aligned_block_allocator&& other ) noexcept = default;
+      aligned_block_allocator( aligned_block_allocator&& other ) = default;
 
       /// \brief Copy-constructs an aligned_block_allocator from another allocator
       ///
       /// \param other the other aligned_block_allocator to copy
-      aligned_block_allocator( const aligned_block_allocator& other ) noexcept = default;
+      aligned_block_allocator( const aligned_block_allocator& other ) = default;
 
       //-----------------------------------------------------------------------
 
@@ -68,13 +156,13 @@ namespace bit {
       ///
       /// \param other the other aligned_block_allocator to move
       /// \return reference to \c (*this)
-      aligned_block_allocator& operator=( aligned_block_allocator&& other ) noexcept = default;
+      aligned_block_allocator& operator=( aligned_block_allocator&& other ) = default;
 
       /// \brief Copy-assigns an aligned_block_allocator from another allocator
       ///
       /// \param other the other aligned_block_allocator to copy
       /// \return reference to \c (*this)
-      aligned_block_allocator& operator=( const aligned_block_allocator& other ) noexcept = default;
+      aligned_block_allocator& operator=( const aligned_block_allocator& other ) = default;
 
       //----------------------------------------------------------------------
       // Block Allocations
@@ -98,6 +186,8 @@ namespace bit {
     // Utilities
     //-------------------------------------------------------------------------
 
+    using dynamic_aligned_block_allocator = aligned_block_allocator<dynamic_size,dynamic_size>;
+
     /// \brief A cached version of aligned_block_allocator
     ///
     /// \tparam Size The size of the block
@@ -110,7 +200,7 @@ namespace bit {
     /// \tparam Size The size of the block
     /// \tparam Align The alignment of block
     template<std::size_t Size,std::size_t Align>
-    using debug_aligned_block_allocator  = debug_block_allocator<aligned_block_allocator<Size,Align>>;
+    using debug_aligned_block_allocator = debug_block_allocator<aligned_block_allocator<Size,Align>>;
 
     /// \brief A cached/debug version of aligned_block_allocator
     ///
@@ -118,6 +208,10 @@ namespace bit {
     /// \tparam Align The alignment of block
     template<std::size_t Size,std::size_t Align>
     using cached_debug_aligned_block_allocator = cached_block_allocator<debug_block_allocator<aligned_block_allocator<Size,Align>>>;
+
+    using cached_dynamic_aligned_block_allocator = cached_block_allocator<dynamic_aligned_block_allocator>;
+    using debug_dynamic_aligned_block_allocator = debug_block_allocator<dynamic_aligned_block_allocator>;
+    using cached_debug_dynamic_aligned_block_allocator = debug_block_allocator<cached_block_allocator<dynamic_aligned_block_allocator>>;
 
   } // namespace memory
 } // namespace bit
