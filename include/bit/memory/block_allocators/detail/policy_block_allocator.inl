e473fb58815a35fd85b75244bb0e3068637e488e
Matthew Rodusek <matthew.rodusek@gmail.com>
2017-10-05 21:56:39 -0400

    Added policy_block_allocator
    This is a policy-based design block allocator, used for debugging support

---
+++ include/bit/memory/block_allocators/detail/policy_block_allocator.inl
@@ -0,0 +1,117 @@
+#ifndef BIT_MEMORY_BLOCK_ALLOCATORS_DETAIL_POLICY_BLOCK_ALLOCATOR_INL
+#define BIT_MEMORY_BLOCK_ALLOCATORS_DETAIL_POLICY_BLOCK_ALLOCATOR_INL
+
+//=============================================================================
+// policy_block_allocator
+//=============================================================================
+
+//-----------------------------------------------------------------------------
+// Constructors / Destructor
+//-----------------------------------------------------------------------------
+
+template<typename BlockAllocator, typename Tagger, typename Tracker, typename Lock>
+template<typename...Args,typename>
+inline bit::memory::policy_block_allocator<BlockAllocator,Tagger,Tracker,Lock>
+  ::policy_block_allocator( Args&&...args )
+  : base_type( std::forward_as_tuple( std::forward<Args>(args)... ),
+               std::make_tuple(),
+               std::make_tuple(),
+               std::make_tuple() )
+{
+
+}
+
+//-----------------------------------------------------------------------------
+
+template<typename BlockAllocator, typename Tagger, typename Tracker, typename Lock>
+inline bit::memory::policy_block_allocator<BlockAllocator,Tagger,Tracker,Lock>
+  ::~policy_block_allocator()
+{
+  auto& allocator = detail::get<0>(*this);
+  auto& tracker   = detail::get<2>(*this);
+
+  tracker.finalize( traits_type::info(allocator) );
+}
+
+//-----------------------------------------------------------------------------
+// Block Allocations
+//-----------------------------------------------------------------------------
+
+template<typename BlockAllocator, typename Tagger, typename Tracker, typename Lock>
+inline bit::memory::owner<bit::memory::memory_block>
+  bit::memory::policy_block_allocator<BlockAllocator,Tagger,Tracker,Lock>
+  ::allocate_block()
+{
+  auto& allocator = detail::get<0>(*this);
+  auto& tagger    = detail::get<1>(*this);
+  auto& tracker   = detail::get<2>(*this);
+  auto& lock      = detail::get<3>(*this);
+
+  auto block = memory_block();
+
+  { // critical section
+    std::lock_guard<lock_type> scope(lock);
+
+    block = traits_type::allocate_block(allocator);
+
+    if( BIT_MEMORY_UNLIKELY( block == nullblock ) ) return nullblock;
+
+    tracker.on_allocate( block.data(), block.size() );
+  }
+
+  tagger.tag_allocation( block.data(), block.size() );
+
+  return block;
+}
+
+//-----------------------------------------------------------------------------
+
+template<typename BlockAllocator, typename Tagger, typename Tracker, typename Lock>
+inline void bit::memory::policy_block_allocator<BlockAllocator,Tagger,Tracker,Lock>
+  ::deallocate_block( owner<memory_block> block )
+{
+  auto& allocator = detail::get<0>(*this);
+  auto& tagger    = detail::get<1>(*this);
+  auto& tracker   = detail::get<2>(*this);
+  auto& lock      = detail::get<3>(*this);
+
+  const auto info = traits_type::info(allocator);
+
+  // Tag the deallocation
+  tagger.tag_deallocation( block.data(), block.size() );
+
+  { // critical section
+    std::lock_guard<lock_type> scope(lock);
+
+    // Untrack the deallocation
+    tracker.on_deallocate( block.data(), block.size() );
+
+    traits_type::deallocate_block( allocator, block );
+  }
+}
+
+//-----------------------------------------------------------------------------
+// Observers
+//-----------------------------------------------------------------------------
+
+template<typename BlockAllocator, typename Tagger, typename Tracker, typename Lock>
+inline const Tracker&
+  bit::memory::policy_block_allocator<BlockAllocator,Tagger,Tracker,Lock>::tracker()
+  const noexcept
+{
+  return detail::get<2>(*this);
+}
+
+//-----------------------------------------------------------------------------
+
+template<typename BlockAllocator, typename Tagger, typename Tracker, typename Lock>
+template<typename,typename>
+inline bit::memory::allocator_info
+  bit::memory::policy_block_allocator<BlockAllocator,Tagger,Tracker,Lock>
+  ::info()
+  const noexcept
+{
+  return traits_type::info( detail::get<0>(*this) );
+}
+
+#endif /* BIT_MEMORY_BLOCK_ALLOCATORS_DETAIL_POLICY_BLOCK_ALLOCATOR_INL */
---
+++ include/bit/memory/block_allocators/policy_block_allocator.hpp
@@ -0,0 +1,201 @@
+/**
+ * \file policy_block_allocator.hpp
+ *
+ * \brief This header contains a block allocator that uses policy-based design
+ *        to componentize entries
+ *
+ * \author Matthew Rodusek (matthew.rodusek@gmail.com)
+ */
+#ifndef BIT_MEMORY_BLOCK_ALLOCATORS_POLICY_BLOCK_ALLOCATOR_HPP
+#define BIT_MEMORY_BLOCK_ALLOCATORS_POLICY_BLOCK_ALLOCATOR_HPP
+
+#include "../detail/ebo_storage.hpp"                     // detail::ebo_storage
+#include "../detail/block_allocator_function_traits.hpp" // detail::block_allocator_has...
+
+#include "../memory_block.hpp"           // memory_block
+#include "../allocator_info.hpp"         // allocator_info
+#include "../block_allocator_traits.hpp" // block_allocator_traits
+
+#include <cstddef> // std::size_t
+#include <tuple>   // std::tuple, std::forward_as_tuple
+#include <mutex>   // std::lock_guard
+
+namespace bit {
+  namespace memory {
+    namespace detail {
+
+      /////////////////////////////////////////////////////////////////////////
+      /// \brief Internal type to conditionally inherit 'block_alignment' and
+      ///        'block_size' from BlockAllocator, if it exists in BlockAllocator
+      ///
+      /////////////////////////////////////////////////////////////////////////
+      template<typename BlockAllocator,
+               bool HasAlignment = block_allocator_has_block_alignment<BlockAllocator>::value,
+               bool HasSize = block_allocator_has_block_size<BlockAllocator>::value>
+      struct policy_block_allocator_base
+      {
+
+      };
+
+      template<typename BlockAllocator, bool HasSize>
+      struct policy_block_allocator_base<BlockAllocator,true,HasSize>
+      {
+        using block_alignment = typename BlockAllocator::block_alignment;
+      };
+
+      template<typename BlockAllocator, bool HasAlignment>
+      struct policy_block_allocator_base<BlockAllocator,HasAlignment,true>
+      {
+        using block_size = typename BlockAllocator::block_size;
+      };
+
+      template<typename BlockAllocator>
+      struct policy_block_allocator_base<BlockAllocator,true,true>
+      {
+        using block_alignment = typename BlockAllocator::block_alignment;
+        using block_size = typename BlockAllocator::block_size;
+      };
+
+    } // namespae detail
+
+
+    ///////////////////////////////////////////////////////////////////////////
+    /// \brief This block allocator uses policies to modify the behaviour of
+    ///        the underlying type
+    ///
+    /// This block allocator acts and behaves identically to the underlying
+    /// BlockAllocator by proxying the same functions defined in the
+    /// BlockAllocator concept.
+    ///
+    /// \satisfies BlockAllocator
+    ///
+    /// \tparam BlockAllocator The block allocator to use
+    /// \tparam MemoryTagger A class used for tagging memory on allocations and
+    ///                      on deallocations.
+    /// \tparam MemoryTracker A class used for tracking memory allocations.
+    /// \tparam BoundsChecker A class used for bounds checking; used to
+    ///                       catch buffer-overflow issues
+    /// \tparam BasicLockable A lockable type for thread-synchronization
+    ///////////////////////////////////////////////////////////////////////////
+    template<typename BlockAllocator,
+             typename MemoryTagger,
+             typename MemoryTracker,
+             typename BasicLockable>
+    class policy_block_allocator
+      : private detail::ebo_storage<BlockAllocator,MemoryTagger,MemoryTracker,BasicLockable>,
+        public detail::policy_block_allocator_base<BlockAllocator>
+    {
+      using traits_type = block_allocator_traits<BlockAllocator>;
+      using base_type = detail::ebo_storage<BlockAllocator,MemoryTagger,MemoryTracker,BasicLockable>;
+
+      //-----------------------------------------------------------------------
+      // Public Member Types
+      //-----------------------------------------------------------------------
+    public:
+
+      using lock_type    = BasicLockable;
+      using tracker_type = MemoryTracker;
+      using is_stateless = std::integral_constant<bool,detail::block_allocator_is_stateless<BlockAllocator>::value &&
+                                                       std::is_empty<MemoryTagger>::value &&
+                                                       std::is_empty<MemoryTracker>::value &&
+                                                       std::is_empty<BasicLockable>::value>;
+
+      //-----------------------------------------------------------------------
+      // Constructors
+      //-----------------------------------------------------------------------
+    public:
+
+      /// \brief Default-constructs the policy_block_allocator
+      policy_block_allocator() = default;
+
+      /// \brief Constructs the underlying block allocator by forwarding the
+      ///        arguments to it
+      ///
+      /// \param args the arguments to forward to the underlying block allocator
+      template<typename...Args, typename = std::enable_if_t<std::is_constructible<BlockAllocator,Args...>::value>>
+      policy_block_allocator( Args&&...args );
+
+      /// \brief Move-constructs this policy_block_allocator from an existing
+      ///        one
+      ///
+      /// \note this is only enabled if the underlying allocator supports it
+      ///
+      /// \param other the other allocator to move
+      policy_block_allocator( policy_block_allocator&& other ) = default;
+
+      /// \brief Copy-constructs this policy_block_allocator from an existing
+      ///        one
+      ///
+      /// \note this is only enabled if the underlying allocator supports it
+      ///
+      /// \param other the other allocator to copy
+      policy_block_allocator( const policy_block_allocator& other ) = default;
+
+      //-----------------------------------------------------------------------
+
+      /// \brief Destructs the policy_block_allocator, finalizing any leak
+      ///        tracking
+      ~policy_block_allocator();
+
+      //-----------------------------------------------------------------------
+
+      /// \brief Move-assigns this policy_block_allocator from an existing
+      ///        one
+      ///
+      /// \note this is only enabled if the underlying allocator supports it
+      ///
+      /// \param other the other allocator to move
+      /// \return reference to \c (*this)
+      policy_block_allocator& operator=( policy_block_allocator&& other ) = default;
+
+      /// \brief Copy-assigns this policy_block_allocator from an existing
+      ///        one
+      ///
+      /// \note this is only enabled if the underlying allocator supports it
+      ///
+      /// \param other the other allocator to copy
+      /// \return reference to \c (*this)
+      policy_block_allocator& operator=( const policy_block_allocator& other ) = default;
+
+      //-----------------------------------------------------------------------
+      // Block Allocation
+      //-----------------------------------------------------------------------
+    public:
+
+      /// \brief Allocates a memory block using the underliny allocator
+      ///
+      /// \return the allocated block
+      owner<memory_block> allocate_block();
+
+      /// \brief Deallocates the specified memory block
+      ///
+      /// \param block the block to deallocate
+      void deallocate_block( owner<memory_block> block );
+
+      //-----------------------------------------------------------------------
+      // Observers
+      //-----------------------------------------------------------------------
+    public:
+
+      /// \brief Accesses the tracker from the arena_allocator
+      ///
+      /// \return the tracker
+      const tracker_type& tracker() const noexcept;
+
+      /// \brief Gets the info about this current block allocator, or null
+      ///        otherwise
+      ///
+      /// \note This is only enabled if the underlying BlockAllocator supports
+      ///       \c info()
+      ///
+      /// \return the allocator info
+      template<typename U = BlockAllocator, typename = std::enable_if_t<detail::block_allocator_has_info<U>::value>>
+      allocator_info info() const noexcept;
+    };
+
+  } // namespace memory
+} // namespace memory
+
+#include "detail/policy_block_allocator.inl"
+
+#endif /* BIT_MEMORY_BLOCK_ALLOCATORS_POLICY_BLOCK_ALLOCATOR_HPP */
