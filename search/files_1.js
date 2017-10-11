396e33a4ddf45b2d048008eb21bfabd40e7880d4
Doxygen Updater <noreply@github.com>
2017-10-10 20:26:42 -0400

    Deploy code docs to GitHub Pages
    Commit: 58bab79f32f476e3447879010ab0f6f8aaf8bfc5

--- search/files_1.js
+++ search/files_1.js
@@ -5,6 +5,7 @@ var searchData=
   ['block_5fallocator_5ftraits_2ehpp',['block_allocator_traits.hpp',['../d9/d89/block__allocator__traits_8hpp.html',1,'']]],
   ['blockallocator_2ehpp',['BlockAllocator.hpp',['../d0/dcd/_block_allocator_8hpp.html',1,'']]],
   ['boundschecker_2ehpp',['BoundsChecker.hpp',['../dc/dd6/_bounds_checker_8hpp.html',1,'']]],
+  ['bump_5fdown_5fallocator_2ehpp',['bump_down_allocator.hpp',['../d4/db1/bump__down__allocator_8hpp.html',1,'']]],
   ['bump_5fdown_5flifo_5fallocator_2ehpp',['bump_down_lifo_allocator.hpp',['../df/deb/bump__down__lifo__allocator_8hpp.html',1,'']]],
   ['bump_5fup_5fallocator_2ehpp',['bump_up_allocator.hpp',['../da/daa/bump__up__allocator_8hpp.html',1,'']]],
   ['bump_5fup_5flifo_5fallocator_2ehpp',['bump_up_lifo_allocator.hpp',['../d1/dcc/bump__up__lifo__allocator_8hpp.html',1,'']]]
